c56082c4ca4e97dc5d8dcfa599933ad32200a614
Simon Massey <322608+simbo1905@users.noreply.github.com>
2025-08-25 22:02:59 +0100

    fix compiler warnings.

--- src/main/java/org/bitbucket/thinbus/junitjs/TestClass.java
+++ src/main/java/org/bitbucket/thinbus/junitjs/TestClass.java
@@ -2,15 +2,29 @@ package org.bitbucket.thinbus.junitjs;
 
 import java.util.List;
 
+/**
+ * Represents a collection of JavaScript test cases from a single test file.
+ */
 public class TestClass {
+	/** The list of test cases in this test class. */
 	public final List<TestCase> testCases;
+	/** The name of the test class. */
 	public final String name;
 
+	/**
+	 * Creates a new test class.
+	 * @param name the name of the test class
+	 * @param testCases the list of test cases in this class
+	 */
 	public TestClass(String name, List<TestCase> testCases) {
 		this.testCases = testCases;
 		this.name = name;
 	}
 
+	/**
+	 * Returns the JUnit-formatted name for this test class.
+	 * @return the JUnit name format
+	 */
     public String junitName() {
         return name.replaceAll("(.*)\\.(.*)","$2.$1");
     }
--- src/main/java/org/bitbucket/thinbus/junitjs/Tests.java
+++ src/main/java/org/bitbucket/thinbus/junitjs/Tests.java
@@ -4,7 +4,14 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 
+/**
+ * Annotation to specify JavaScript test files to run.
+ */
 @Retention(RUNTIME)
 public @interface Tests {
+	/**
+	 * Array of JavaScript test file names to execute.
+	 * @return the test file names
+	 */
 	String[] value();
 }
